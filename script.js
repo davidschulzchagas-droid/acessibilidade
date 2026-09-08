04ea16caa5bb02b4795e2ae5689079ce76d1c4a7
davidschulzchagas-droid <david.schulz.chagas@escola.pr.gov.br>
2026-09-08 08:34:59 -0300

    rtty

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-let btAjuda = document.querySelectory(".botao-ajuda");
-let btFechar = document .querySelectory(".botao-ajuda");
-let modal = document .querySelectory(".botao-ajuda");
+let btnAjuda = document.querySelectory(".botao-help");
+let btnFechar = document .querySelectory(".botao-close");
+let modal = document .querySelectory(".modal-fundo");
+
+btnAjuda.addEventlistener("clike", abreModal);
+btnFechar.addEventlistener("clike", fechaModal);
+
+function abreModal() {
+    modal.style.display = "block";
+}
+
+function fechaModal() {
+    modal.style.display = "none";
+}
